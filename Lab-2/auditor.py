1940cf9508f5694f3a33305cc6a84c928a9eac0b
Jun Qi <2605575@sit.singaporetech.edu.sg>
2026-09-11 13:44:38 +0800

    IMplemented validation for invalid input and enforced business logic

--- Lab-2/auditor.py
+++ Lab-2/auditor.py
@@ -1,13 +1,21 @@
 inventory = 0
 
 while True:
+    user_input = input("Enter stock quantity (or type 'quit' to exit): ")
+
+    if user_input == 'quit':
+        print("Exiting the program.")
+        break    
+
+    # Checks if the input is a valid integer ignoring the negative sign for validation
+    # Afterwards converts user input string to an integer and checks if it is non-negative
     try:
-        user_input = input("Enter stock quantity (or type 'quit' to exit): ")
-        if user_input == 'quit':
-            print("Exiting the program.")
-            break    
-    except ValueError:
+        if not user_input.lstrip('-').isdigit():
+            print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
+        elif int(user_input) < 0:
+            print("Invalid input. Please enter a non-negative integer.")
+    except ValueError: # Exception to catch any unexpected errors during conversion e.g. --3
         print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
-        continue
+     
 
     
