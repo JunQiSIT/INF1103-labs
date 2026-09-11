71d7bfa0e06c85507ebaa3847e66a5b2d637428e
Jun Qi <2605575@sit.singaporetech.edu.sg>
2026-09-11 13:59:17 +0800

    Implemented reporting, state of inventory and overstock alert

--- Lab-2/auditor.py
+++ Lab-2/auditor.py
@@ -1,10 +1,16 @@
 inventory = 0
+failed_entries = 0
 
+
+print('Welcome to the Smart Inventory Auditor!')
 while True:
+    add_inventory = True
     user_input = input("Enter stock quantity (or type 'quit' to exit): ")
 
     if user_input == 'quit':
-        print("Exiting the program.")
+        print("Exiting Smart Inventory Auditor.")
+        print(f'Total Units Processed: {inventory}')
+        print(f'Number of Failed/Rejected entries: {failed_entries}')
         break    
 
     # Checks if the input is a valid integer ignoring the negative sign for validation
@@ -12,10 +18,23 @@ while True:
     try:
         if not user_input.lstrip('-').isdigit():
             print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
+            failed_entries += 1
+            add_inventory = False
         elif int(user_input) < 0:
             print("Invalid input. Please enter a non-negative integer.")
+            failed_entries += 1
+            add_inventory = False
+
+        # If input is validated proceed to add inventory
+        if add_inventory:
+            # Check if inventory exceed limit of 500 units
+            if inventory > 500:
+                print("Overstock inventory limit exceeded. Cannot add more stock.")
+                break
+            else:
+                inventory += int(user_input)
+                print(f'Current Inventory: {inventory}')
     except ValueError: # Exception to catch any unexpected errors during conversion e.g. --3
         print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
-     
-
-    
+        failed_entries += 1
+        add_inventory = False
