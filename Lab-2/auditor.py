f33bf8aa28d2a42198cb52278eacbc866effb06f
Jun Qi <2605575@sit.singaporetech.edu.sg>
2026-09-11 12:10:35 +0800

    Initial conditional loop creation with partial dat a validation

--- Lab-2/auditor.py
+++ Lab-2/auditor.py
@@ -1,1 +1,13 @@
-print('hello world')
+inventory = 0
+
+while True:
+    try:
+        user_input = input("Enter stock quantity (or type 'quit' to exit): ")
+        if user_input == 'quit':
+            print("Exiting the program.")
+            break    
+    except ValueError:
+        print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
+        continue
+
+    
