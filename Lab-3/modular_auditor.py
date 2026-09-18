d21eac064478a02c233ef85c4c05e909249866a5
Jun Qi <2605575@sit.singaporetech.edu.sg>
2026-09-18 20:14:45 +0800

    Lab 3 Added get_valid_input function and tested validation

--- Lab-3/modular_auditor.py
+++ Lab-3/modular_auditor.py
@@ -1,8 +1,34 @@
+# Global Variables
 inventory = 0
 failed_entries = 0
 
+# Functions
 def get_valid_input():
-    return
+    global failed_entries
+    user_input = input("Enter stock quantity (or type 'quit' to exit): ")
+
+    # Checks if input is quit signal to exit the program
+    if user_input == 'quit':
+        return user_input, False, failed_entries
+
+    try:
+        if not user_input.lstrip('-').isdigit():
+            print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
+
+            failed_entries += 1
+            return user_input, False, failed_entries
+        elif int(user_input) < 0:
+            print("Invalid input. Please enter a non-negative integer.")
+
+            failed_entries += 1
+            return user_input, False, failed_entries
+
+    except ValueError: # Exception to catch any unexpected errors during conversion e.g. --3
+        print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
+        failed_entries += 1
+        return user_input, False, failed_entries
+
+    return user_input, True, failed_entries
 
 def process_delivery(current_total, new_value):
     return
@@ -13,39 +39,11 @@ def calculate_tax(amount):
 def generate_report(total_units,failed_attempts):
     return
 
+# Main Program
 print('Welcome to the Smart Inventory Auditor!')
 while True:
     add_inventory = True
-    user_input = input("Enter stock quantity (or type 'quit' to exit): ")
-
-    if user_input == 'quit':
-        print("Exiting Smart Inventory Auditor.")
-        print(f'Total Units Processed: {inventory}')
-        print(f'Number of Failed/Rejected entries: {failed_entries}')
-        break    
-
-    # Checks if the input is a valid integer ignoring the negative sign for validation
-    # Afterwards converts user input string to an integer and checks if it is non-negative
-    try:
-        if not user_input.lstrip('-').isdigit():
-            print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
-            failed_entries += 1
-            add_inventory = False
-        elif int(user_input) < 0:
-            print("Invalid input. Please enter a non-negative integer.")
-            failed_entries += 1
-            add_inventory = False
-
-        # If input is validated proceed to add inventory
-        if add_inventory:
-            # Check if inventory exceed limit of 500 units
-            if inventory > 500:
-                print("Overstock inventory limit exceeded. Cannot add more stock.")
-                break
-            else:
-                inventory += int(user_input)
-                print(f'Current Inventory: {inventory}')
-    except ValueError: # Exception to catch any unexpected errors during conversion e.g. --3
-        print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
-        failed_entries += 1
-        add_inventory = False
+    user_input, add_inventory, failed_entries = get_valid_input()
+    print(user_input)
+    print(add_inventory)
+    print(failed_entries)
