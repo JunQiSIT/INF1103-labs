899383c02dd87e31aff901251a008f0d0f363033
Jun Qi <2605575@sit.singaporetech.edu.sg>
2026-09-25 14:27:00 +0800

    Lab 4: removed unused functions based on new requirement, added load_inventory and save_inventory function and product name prompt

--- Lab-4/persistent_auditor.py
+++ Lab-4/persistent_auditor.py
@@ -1,3 +1,5 @@
+from pathlib import Path    
+
 # Global Variables
 inventory = 0
 failed_entries = 0
@@ -7,11 +9,12 @@ failed_entries = 0
 def get_valid_input():
     global failed_entries
     global add_inventory
+    product_name = input("Enter Product Name (or type 'quit' to exit): ")
     user_input = input("Enter stock quantity (or type 'quit' to exit): ")
 
     # Checks if input is quit signal to exit the program
-    if user_input == 'quit':
-        return user_input, False, failed_entries
+    if user_input == 'quit' or product_name == 'quit':
+        return product_name, user_input, False, failed_entries
 
     # Checks if the input is a valid integer ignoring the negative sign for validation
     # Afterwards converts user input string to an integer and checks if it is non-negative
@@ -19,27 +22,23 @@ def get_valid_input():
         if not user_input.lstrip('-').isdigit():
             print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
             failed_entries += 1
-            return user_input, False, failed_entries
+            return product_name, user_input, False, failed_entries
         elif int(user_input) < 0:
             print("Invalid input. Please enter a non-negative integer.")
             failed_entries += 1
-            return user_input, False, failed_entries
+            return product_name, user_input, False, failed_entries
 
     except ValueError: # Exception to catch any unexpected errors during conversion e.g. --3
         print("Invalid input. Please enter a valid integer or type 'quit' to exit.")
         failed_entries += 1
-        return user_input, False, failed_entries
+        return product_name, user_input, False, failed_entries
 
     # Return user input if it passes validation
-    return int(user_input), True, failed_entries
+    return product_name, int(user_input), True, failed_entries
 
 # Processes the delivery by adding the new value to the current total inventory
 def process_delivery(current_total, new_value):
     return current_total + new_value
-
-# Calculates 10% tax on the delivery amount
-def calculate_tax(amount):
-    return amount * 0.1
 
 # Generates report of total deliveries processed and number of failed/rejected entries
 def generate_report(total_units,failed_attempts):
@@ -47,19 +46,36 @@ def generate_report(total_units,failed_attempts):
     print(f'Number of Failed/Rejected Entries: {failed_attempts}')
     return
 
+# Load inventory from file
+def load_inventory():
+    filepath = "./orders.txt"
+
+    return
+
+# Save inventory to file for persistence
+def save_inventory():
+    return
+
+
 # Main Program
-print('Welcome to the Smart Inventory Auditor!')
-while True:
-    add_inventory = True
-    user_input, add_inventory, failed_entries = get_valid_input()
+def main():
+    print('Welcome to the Smart Inventory Auditor!')
+    # Initialise inventory from file
+    load_inventory()
+    while True:
+        add_inventory = True
+        product_input, user_input, add_inventory, failed_entries = get_valid_input()
 
-    if user_input == 'quit':
-        print("\nExiting Smart Inventory Auditor.")
-        generate_report(inventory, failed_entries)
-        break
+        if user_input == 'quit' or product_input == 'quit':
+            print("\nExiting Smart Inventory Auditor.")
+            generate_report(inventory, failed_entries)
+            break
 
-    if add_inventory:
-        inventory = process_delivery(inventory, user_input)
-        tax = calculate_tax(user_input)
-        print(f'\nDelivery processed: {user_input} units, Tax: ${tax:.2f}')
-        print(f'Current Inventory: {inventory}')
+        if add_inventory:
+            return
+
+    # Save recorded inventory into file for persistence
+    save_inventory()
+    
+if __name__=="__main__":
+    main()
